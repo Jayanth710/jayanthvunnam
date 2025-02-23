f13ce47b034d954023c8641b223f567c1beac6cb
Jayanth710 <vunnamjayanth7@gmail.com>
2025-02-23 16:10:43 -0700

    Added Services folder in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
-import Services from "./components/services/services";
+import Services from "./components/Services/Services";
 import Mywork from "./components/Mywork/Mywork";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
